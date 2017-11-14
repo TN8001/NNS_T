14572d5a87e7dcd4445655c5d5e35bb3b26058ab
TN8001 <naga8001@hotmail.com>
2017-11-15 00:03:50 +0900

    v1.0.2

--- NNS_T/Models/Enums/FolderType.cs
+++ NNS_T/Models/Enums/FolderType.cs
@@ -1,9 +1,13 @@
 ﻿
 namespace NNS_T.Models
 {
+    /// <summary>開くフォルダ種類</summary>
     public enum FolderType
     {
+        /// <summary>exeのあるフォルダ</summary>
         Assembly,
+
+        /// <summary>user.configのあるフォルダ</summary>
         Settings,
     }
 }
